da06333093c46dca8a0da4cd40a3cc64ea09ea35
jake-dwyer <dwyer.jak@northeastern.edu>
2025-01-22 21:01:07 -0500

    lab exercises css

---
+++ src/Labs/Lab2/Borders.tsx
@@ -0,0 +1,11 @@
+const Borders = () => {
+    return (
+        <div id="wd-css-borders">
+            <h2>Borders</h2>
+            <p className="wd-border-fat wd-border-red">Solid fat red border</p>
+            <p className="wd-border-thin wd-border-blue wd-border-dashed">Dashed thin blue border</p>
+        </div>
+    );
+}
+
+export default Borders;
--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,3 +1,53 @@
+import "./index.css";
+import BackgroundColors from "./BackgroundColors";
+import Borders from "./Borders";
+import Corners from "./Corners";
+import ForegroundColors from "./ForegroundColors";
+import GridLayout from "./GridLayout";
+import Margins from "./Margins";
+import Padding from "./Padding";
+import Positions from "./Positions";
+import Zindex from "./Zindex";
+import Flex from "./Flex";
+
 export default function Lab2() {
-    return <h2>Lab 2</h2>
+    return (
+        <div id="wd-lab2">
+            <h2>Lab 2 - Cascading Style Sheets</h2>
+            <h3>Styling with the STYLE attribute</h3>
+            <p>
+                Style attribute allows configuring look and feel right on the element. Although it's very convenient, it is considered bad practice and you should avoid using the style attribute.
+            </p>
+            <div>
+                <BackgroundColors />
+            </div>
+            <div>
+                <Borders />
+            </div>
+            <div>
+                <Corners />
+            </div>
+            <div>
+                <ForegroundColors />
+            </div>
+            <div>
+                <GridLayout />
+            </div>
+            <div>
+                <Margins />
+            </div>
+            <div>
+                <Padding />
+            </div>
+            <div>
+                <Positions />
+            </div>
+            <div>
+                <Zindex />
+            </div>
+            <div>
+                <Flex />
+            </div>
+        </div>
+    );
 }
